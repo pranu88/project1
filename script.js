03ccb5b970e0e1beb026b1cb15bd47dc05941729
pranu88 <pranusha.pothugunta@gmail.com>
2023-11-07 11:19:56 -0500

    rollcompleted

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // snake and ladder 
+// queries
+let dicevalue = document.querySelector('#dicevalue')
+
+// function for dice roll
+function roll(){
+    let num = Math.floor(Math.random()*(6 - 1)+ 1)    
+    dicevalue.innerHTML = num;
+}
 
 
 
