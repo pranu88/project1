ed64e9ea1077265c8f5b2c88534628aec37e25fd
pranu88 <pranusha.pothugunta@gmail.com>
2023-11-07 19:18:54 -0500

    roll updated

--- script.js
+++ script.js
@@ -3,64 +3,82 @@
 let dicevalue = document.querySelector('#dicevalue')
 let dicebtn = document.querySelector('.dice')
 let cPlayer = document.querySelector('#cp')
-let p1 = document.getElementById('player1').value
-let p2 = document.getElementById('player2').value
+// let p1 = document.getElementById('player1').value
+// let p2 = document.getElementById('player2').value
 let msg = document.querySelector('#message')
 //  snakes and ladders objects
-let snakes = {
+const snakes = {
      34:13,
      21:7
     }
-let ladders = {
+const ladders = {
     3:11,
     19:31
     }
 // player objects
-let player1 = {
- name: p1,
+const player1 = {
+ name: 'p1',
  position:0
 }
-let player2 = {
-    name: p2,
+const player2 = {
+    name: 'p2',
     position:0
 }
-let currentPlayer = player1
+
 // function for displaying message
 function message(alert){
 msg.textContent = alert
 }
+
+let currentPlayer = player1
 // function for dice roll
 function roll(){
+    let p1 = document.getElementById('player1').value
+let p2 = document.getElementById('player2').value
+  player1.name = p1
+  player2.name = p2
     let num = Math.floor(Math.random()*(6 - 1)+ 1)    
     dicevalue.innerHTML = num;
     play(currentPlayer,num)
+    if(currentPlayer.position >= 36){
+        message(`${currentPlayer.name} is winner!`)
+        dicebtn.disabled = true
+    }
+    else{
+        currentPlayer = currentPlayer === player1 ? player2 : player1
+    }
+
+
 }
    // function for play
  
    function play(player,moves){
+    cPlayer.textContent = `${currentPlayer.name} turn`
      player.position += moves
-     if(player.position <= 36){
+    //  if(player.position <= 36){
      if (player.position in snakes){
         player.position = snakes[player.position]
         // display message encounterd snake and moved backwards
-        message(`${player} encountered snake moving back to ${player.position}`)
+        message(`${player.name} encountered snake moving back to ${player.position}`)
      }
      else if(player.position in ladders){
         player.position = ladders[player.position]
         // display message climbed ladder and moved forward
-        message(`${player} climbed ladder moved to ${player.position}`)
+        message(`${player.name} climbed ladder moved to ${player.position}`)
      }
      else {
         player.position = player.position
         // diplay player at position ${player.position}
-        message(`${player} is at ${player.position}`)
+        message(`${player.name} is at ${player.position}`)
+        // cPlayer.textContent = `${currentPlayer.name} turn`
      }
     }
-   }
+//    }
 
-roll()
 
-dicebtn.addEventListener('click',roll)
+dicebtn.addEventListener('click', () => {
+    roll()
+})
 
 
 
